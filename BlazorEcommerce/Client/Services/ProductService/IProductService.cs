a4576c801cf56cbddeebd7d6e03fed4caa3ef2b1
Vially Israel <viallyisrael@hotmail.com>
2022-04-05 20:29:34 -0300

    pagination finished

--- BlazorEcommerce/Client/Services/ProductService/IProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/IProductService.cs
@@ -2,13 +2,17 @@
 {
     public interface IProductService
     {
+        int CurrentPage { get; set; }
+        int PageCount { get; set; }
+        string LastSearchText { get; set; }
+
         string Message { get; set; }
 
         event Action ProductChanged;
         List<Product> Products { get; set; }
         Task GetProducts(string? categoryUrl = null);
         Task<ServiceResponse<Product>> GetProduct(int id);
-        Task SearchProducts(string searchText);
+        Task SearchProducts(string searchText, int page);
         Task<List<string>> GetProductsSearchSuggestions(string searchText);
 
     }
