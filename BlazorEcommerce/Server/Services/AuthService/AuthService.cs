5ff026ab566e8e4be2419f79763aec5d2885915f
Vially Israel <viallyisrael@hotmail.com>
2022-04-20 13:45:42 -0300

    login route

--- BlazorEcommerce/Server/Services/AuthService/AuthService.cs
+++ BlazorEcommerce/Server/Services/AuthService/AuthService.cs
@@ -1,14 +1,43 @@
-﻿using System.Security.Cryptography;
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
 
 namespace BlazorEcommerce.Server.Services.AuthService
 {
     public class AuthService : IAuthService
     {
         private readonly DataContext _context;
+        private readonly IConfiguration _configuration;
 
-        public AuthService(DataContext context)
+        public AuthService(DataContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
+        }
+
+        public async Task<ServiceResponse<string>> Login(string email, string password)
+        {
+            var response = new ServiceResponse<string>();
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email.ToLower().Equals(email.ToLower()));
+
+            if (user == null)
+            {
+                response.Success = false;
+                response.Message = "User not found";
+            }
+            else if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+            {
+                response.Success = false;
+                response.Message = "Wrong password";
+            }
+            else
+            {
+                response.Data = CreateToken(user);
+
+            }
+
+            return response;
         }
 
 
@@ -16,9 +45,10 @@ namespace BlazorEcommerce.Server.Services.AuthService
         {
             if (await UserExists(user.Email))
             {
-                return new ServiceResponse<int> { 
+                return new ServiceResponse<int>
+                {
                     Success = false,
-                    Message="User already exists" 
+                    Message = "User already exists"
                 };
             }
 
@@ -31,7 +61,7 @@ namespace BlazorEcommerce.Server.Services.AuthService
 
             await _context.SaveChangesAsync();
 
-            return new ServiceResponse<int> { Data = user.Id, Message="Registration Successfull" };
+            return new ServiceResponse<int> { Data = user.Id, Message = "Registration Successfull" };
         }
 
         public async Task<bool> UserExists(string email)
@@ -46,10 +76,41 @@ namespace BlazorEcommerce.Server.Services.AuthService
 
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
-            using(var hmac = new HMACSHA512())
+            using (var hmac = new HMACSHA512())
             {
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+        private string? CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Email),
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                        claims: claims, 
+                        expires: DateTime.Now.AddDays(1),
+                        signingCredentials: creds);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computerHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+                return computerHash.SequenceEqual(passwordHash);
             }
         }
     }
