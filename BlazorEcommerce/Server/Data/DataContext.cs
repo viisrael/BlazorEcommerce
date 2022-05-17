408ddffb40ffdbdf17926244bb460c650bfeb5e6
Vially Israel <viallyisrael@hotmail.com>
2022-05-17 20:15:34 -0300

    Creating the address model and the migration

--- BlazorEcommerce/Server/Data/DataContext.cs
+++ BlazorEcommerce/Server/Data/DataContext.cs
@@ -6,6 +6,7 @@ namespace BlazorEcommerce.Server.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Address> Addresses { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
